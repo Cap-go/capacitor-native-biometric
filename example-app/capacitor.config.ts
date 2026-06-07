495da71d3fd8aeeb2955b79c2fa373d95a315160
Martin Donadieu <martindonadieu@gmail.com>
2026-06-07 13:13:40 +0200

    style: format capgo example rollout

--- example-app/capacitor.config.ts
+++ example-app/capacitor.config.ts
@@ -3,24 +3,24 @@ import type { CapacitorConfig } from '@capacitor/cli';
 import pkg from './package.json';
 
 const config: CapacitorConfig = {
-  "appId": "app.capgo.native.biometric",
-  "appName": "example-app",
-  "webDir": "dist",
-  "plugins": {
-    "SplashScreen": {
-      "launchAutoHide": true,
-      "launchShowDuration": 1000
+  appId: 'app.capgo.native.biometric',
+  appName: 'example-app',
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchAutoHide: true,
+      launchShowDuration: 1000,
     },
-    "CapgoNativeBiometric": {},
-    "CapacitorUpdater": {
-      "appId": "app.capgo.native.biometric",
-      "autoUpdate": true,
-      "autoSplashscreen": true,
-      "directUpdate": "always",
-      "defaultChannel": "production",
-      "version": pkg.version
-    }
-  }
+    CapgoNativeBiometric: {},
+    CapacitorUpdater: {
+      appId: 'app.capgo.native.biometric',
+      autoUpdate: true,
+      autoSplashscreen: true,
+      directUpdate: 'always',
+      defaultChannel: 'production',
+      version: pkg.version,
+    },
+  },
 };
 
 export default config;
